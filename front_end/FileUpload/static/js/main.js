e3636f59cd8412f5425625fe730c7191899247de
Zachary Yocum <zachary.yocum@me.com>
2014-11-30 20:21:25 -0500

    Moved enter-key event out of main

--- front_end/FileUpload/static/js/main.js
+++ front_end/FileUpload/static/js/main.js
@@ -1,22 +1,4 @@
 $(function () {
-
-    var submitenter = function(myfield,e) {
-        var keycode;
-        if (window.event) {
-            keycode = window.event.keyCode;
-        } else if (e) {
-            keycode = e.which;
-        } else {
-            return true;
-        };
-        if (keycode == 13) {
-            myfield.submit();
-            return false;
-        } else {
-            return true
-        };
-    }
-    
   var startRecorder = function(recorder) {
     recorder.clear();
     recorder.record();
